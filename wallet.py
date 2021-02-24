f1ed5ee5a8d87c8ec2ae46c2e5d6d4468cb3b26e
JTMCGINLEY123 <jtmcginley123@gmail.com>
2021-02-24 06:17:24 -0600

    lets goooo

--- wallet.py
+++ wallet.py
@@ -3,11 +3,9 @@ import json
 from constants import *
 import os
 
-mnemonic = os.getenv('MNEMONIC', 'insert mnemonic here')
 
-def derive_wallets (mnemonic, coin, numderive):
 
-command = './derive -g --mnemonic="INSERT HERE" --cols=path,address,privkey,pubkey --format=json'
+command = './derive -g --mnemonic="decide wage cotton law laundry renew practice olympic sound spread target gap" --cols=path,address,privkey,pubkey --format=json'
 
 p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
 output, err = p.communicate()
