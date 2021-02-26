54f8317205d7a29c1c85839a0421e877f81fc3bb
JTMCGINLEY123 <jtmcginley123@gmail.com>
2021-02-26 02:39:30 -0600

    meep

--- wallet.py
+++ wallet.py
@@ -23,7 +23,7 @@ def derive_wallets(mnemonic, numderive, coin):
     keys = json.loads(output)
     print(keys)
 
-coins = {'eth':derive_wallets(mnemonic=mnemonic, coin=ETH, numderive=3), 'btc-test':derive_wallets(mnemonic=mnemonic,coin=BTCTEST,numderive=3)}
+coins = {'eth':derive_wallets(ETH), 'btc-test':derive_wallets(BTCTEST)}
 
 eth_privatekey = coins['eth'][0]['privkey']
 btc_privatekey = coins['btc-test']['privkey']
