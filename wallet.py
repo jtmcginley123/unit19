bfcbca93689d426f1731240988445ec4212cfb74
JTMCGINLEY123 <jtmcginley123@gmail.com>
2021-02-26 02:36:44 -0600

    change location

--- wallet.py
+++ wallet.py
@@ -20,10 +20,8 @@ def derive_wallets(mnemonic, numderive, coin):
     p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
     output, err = p.communicate()
     p_status = p.wait()
-    return json.loads(output)
-
-keys = json.loads(output)
-print(keys)
+    keys = json.loads(output)
+    print(keys)
 
 coins = {'eth':derive_wallets(mnemonic=mnemonic, coin=ETH, numderive=3), 'btc-test':derive_wallets(mnemonic=mnemonic,coin=BTCTEST,numderive=3)}
 
