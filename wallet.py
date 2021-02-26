ca8cba5ea5fd0d077c6b77340bc0d50ac54abc75
JTMCGINLEY123 <jtmcginley123@gmail.com>
2021-02-26 01:32:56 -0600

    almost there

--- wallet.py
+++ wallet.py
@@ -1,8 +1,8 @@
 import subprocess
 import json
 from dotenv import load_dotenv
-from web3 import Web3
 import os
+from web3 import Web3
 from constants import *
 from eth_account import Account
 from bit import *
