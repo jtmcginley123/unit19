334fcf05de7813b905f87a823fa2aae1d9be926d
JTMCGINLEY123 <jtmcginley123@gmail.com>
2021-02-26 22:44:34 -0600

    disconnect

--- wallet.py
+++ wallet.py
@@ -4,14 +4,14 @@ from constants import *
 from dotenv import load_dotenv
 from web3 import Web3
 import os
-from eth_account import Account
 from bit import wif_to_key
 from bit.network import NetworkAPI
+from eth_account import Account
 
 load_dotenv()
 
 mnemonic = os.getenv('MNEMONIC')
-privkey = os.getenv("PRIVATE_KEY")
+privkey = os.getenv('PRIVATE_KEY')
 w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
 
 
@@ -23,18 +23,18 @@ def derive_wallets(mnemonic, coin, numderive):
     p_status = p.wait()
     return json.loads(output)
 
-
 coins = {'eth':derive_wallets(mnemonic=mnemonic,coin=ETH,numderive=3), 'btc-test':derive_wallets(mnemonic=mnemonic,coin=BTCTEST,numderive=3)}
 
 
-def priv_key_to_account (coin, privkey):
+def priv_key_to_account (coin, priv_key):
     if coin == ETH:
-        return Account.privateKeyToAccount(privkey)
+        return Account.privateKeyToAccount(priv_key)
     if coin == BTCTEST:
-        return PrivateKeyTestnet(privkey)
+        return PrivateKeyTestnet(priv_key)
 
 eth_privatekey = coins['eth'][0]['privkey']
 btc_privatekey = coins['btc-test'][0]['privkey']
+
 
 def create_tx (coin, account, to, amount):
     if coin == 'ETH':
